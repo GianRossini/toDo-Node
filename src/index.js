74bf9bb6cf4b76fcee147334a09f13deca15992f
GianRossini <gianrossini.developer@gmail.com>
2022-03-06 14:13:00 -0300

    creates middleware and create user route

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
 
-app.post('/users', (request, response) => {
-  // Complete aqui
-});
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User doesn't exist." });
+  }
+
+  request.user = user;
+
+  return next();
+}
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+});
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  if (users.some(user => user.username === username)) {
+    return response.status(400).json({ error: "User already exists." });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.json(user);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
